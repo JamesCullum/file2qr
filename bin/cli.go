e75dff0eb23091868b22fdfc7970f5bbb001e0a2
James Cullum (Pseudonym) <JamesCullum@users.noreply.github.com>
2019-05-01 19:47:14 +0200

    Improved lint & documentation
    Converter struct changed to public to make it available to godoc

--- bin/cli.go
+++ bin/cli.go
@@ -29,46 +29,46 @@ func main() {
 	}
 
 	// Init progressbar
-	current_progress := 0
+	currentProgress := 0
 	progress := progressbar.New(100)
-	go update_progressbar(&current_progress, progress)
+	go updateProgressbar(&currentProgress, progress)
 
 	// Init handler
 	handler := file2qr.New()
 
 	// Encode
 	if *flagEncode == true {
-		err := handler.Encode(*flagEncodeSrc, *flagEncodeDst, &current_progress)
+		err := handler.Encode(*flagEncodeSrc, *flagEncodeDst, &currentProgress)
 		if err != nil {
 			log.Panic("Encoding failed: ", err)
 		}
-		current_progress = 100
+		currentProgress = 100
 		progress.Finish()
 	}
 
 	// Reset progressbar
 	if *flagEncode == true && *flagDecode == true {
-		current_progress = 0
+		currentProgress = 0
 		progress.Reset()
-		go update_progressbar(&current_progress, progress)
+		go updateProgressbar(&currentProgress, progress)
 	}
 
 	// Decode
 	if *flagDecode == true {
-		err := handler.Decode(*flagDecodeSrc, *flagDecodeDst, &current_progress)
+		err := handler.Decode(*flagDecodeSrc, *flagDecodeDst, &currentProgress)
 		if err != nil {
 			log.Panic("Decoding failed: ", err)
 		}
-		current_progress = 100
+		currentProgress = 100
 		progress.Finish()
 	}
 }
 
-func update_progressbar(current_progress *int, progress *progressbar.ProgressBar) {
+func updateProgressbar(currentProgress *int, progress *progressbar.ProgressBar) {
 	for range time.Tick(1 * time.Second) {
-		progress.Set(*current_progress)
+		progress.Set(*currentProgress)
 
-		if *current_progress >= 100 {
+		if *currentProgress >= 100 {
 			progress.Finish()
 			return
 		}
--- file2qr.go
+++ file2qr.go
@@ -16,7 +16,7 @@ import (
 	"github.com/makiuchi-d/gozxing/qrcode"
 )
 
-type converter struct {
+type Converter struct {
 	NumThread         int
 	QRChunkSize       int64
 	QRSideLength      int
@@ -26,8 +26,8 @@ type converter struct {
 
 // New creates a converter instance with default settings.
 // You can directly manipulate the available parameters to modify settings.
-func New() *converter {
-	return &converter{
+func New() *Converter {
+	return &Converter{
 		NumThread:         25,
 		QRChunkSize:       2950,
 		QRSideLength:      1480,
@@ -38,66 +38,66 @@ func New() *converter {
 
 // Encode reads the input file and converts it to QR code images in the destination folder.
 // It writes the current progress percentage into the provided progress reference integer.
-func (c *converter) Encode(input_filepath, output_folderpath string, progress *int) error {
+func (c *Converter) Encode(inputFilepath, outputFolderpath string, progress *int) error {
 	// Open up input file. Will be read in batches to reduce memory impact.
-	input_file, err := os.Open(input_filepath)
+	inputFile, err := os.Open(inputFilepath)
 	if err != nil {
 		return err
 	}
-	defer input_file.Close()
+	defer inputFile.Close()
 
-	file_stat, err := input_file.Stat()
+	fileStat, err := inputFile.Stat()
 	if err != nil {
 		return err
 	}
 
 	// Create folder if it does not exist
-	os.Mkdir(output_folderpath, 0644)
+	os.Mkdir(outputFolderpath, 0644)
 
 	// Init encoder
 	encoder := qrcode.NewQRCodeWriter()
-	encode_hints := map[gozxing.EncodeHintType]interface{}{gozxing.EncodeHintType_ERROR_CORRECTION: "L", gozxing.EncodeHintType_CHARACTER_SET: "ASCII"}
+	encodeHints := map[gozxing.EncodeHintType]interface{}{gozxing.EncodeHintType_ERROR_CORRECTION: "L", gozxing.EncodeHintType_CHARACTER_SET: "ASCII"}
 
 	*progress = 0
-	max_i := int(math.Floor(float64(file_stat.Size())/float64(c.QRChunkSize))) + 1
-	read_buffer := make([]byte, c.QRChunkSize)
+	maxI := int(math.Floor(float64(fileStat.Size())/float64(c.QRChunkSize))) + 1
+	readBuffer := make([]byte, c.QRChunkSize)
 
 	sem := make(chan bool, c.NumThread)
-	for i := 1; i <= max_i; i++ {
+	for i := 1; i <= maxI; i++ {
 		sem <- true
 
 		// Read chunk
-		readBytes, err := input_file.Read(read_buffer)
+		readBytes, err := inputFile.Read(readBuffer)
 		if err != nil {
 			return err
 		}
 
 		// Hand over to thread to encode and write to file
 		go func(i int, chunk string) {
-			qr_image, err := encoder.Encode(chunk, gozxing.BarcodeFormat_QR_CODE, c.QRSideLength, c.QRSideLength, encode_hints)
+			qrImage, err := encoder.Encode(chunk, gozxing.BarcodeFormat_QR_CODE, c.QRSideLength, c.QRSideLength, encodeHints)
 			if err != nil {
 				panic(err)
 			}
 
 			filename := c.FilePrefix + lpad(strconv.Itoa(i), "0", c.FilenamePadLength) + ".png"
-			f, err := os.Create(output_folderpath + string(os.PathSeparator) + filename)
+			f, err := os.Create(outputFolderpath + string(os.PathSeparator) + filename)
 			if err != nil {
 				panic(err)
 			}
 			defer f.Close()
 
-			if err := png.Encode(f, qr_image); err != nil {
+			if err := png.Encode(f, qrImage); err != nil {
 				panic(err)
 			}
 
 			// Update progress
-			tempProgress := int((i * 100) / max_i)
+			tempProgress := int((i * 100) / maxI)
 			if tempProgress > *progress {
 				*progress = tempProgress
 			}
 
 			<-sem
-		}(i, string(read_buffer[:readBytes]))
+		}(i, string(readBuffer[:readBytes]))
 	}
 
 	// Wait for all threads
@@ -111,48 +111,48 @@ func (c *converter) Encode(input_filepath, output_folderpath string, progress *i
 // Decode reads qr codes from a source folder, decodes them and writes the result in the destination file.
 // The source folder should only contain images that belong to one file.
 // It writes the current progress percentage into the provided progress reference integer.
-func (c *converter) Decode(input_folderpath, output_filepath string, progress *int) error {
+func (c *Converter) Decode(inputFolderpath, outputFilepath string, progress *int) error {
 	// Init decoder twice with different settings as fallback.
 	// See https://github.com/makiuchi-d/gozxing/issues/14#issuecomment-486279433 for details.
 	decoder := qrcode.NewQRCodeReader()
-	decode_hints := make(map[gozxing.DecodeHintType]interface{})
-	decode_hints[gozxing.DecodeHintType_PURE_BARCODE] = true
-	decode_hints2 := make(map[gozxing.DecodeHintType]interface{})
+	decodeHints := make(map[gozxing.DecodeHintType]interface{})
+	decodeHints[gozxing.DecodeHintType_PURE_BARCODE] = true
+	decodeHints2 := make(map[gozxing.DecodeHintType]interface{})
 
-	output_file, err := os.Create(output_filepath)
+	outputFile, err := os.Create(outputFilepath)
 	if err != nil {
 		return err
 	}
-	defer output_file.Close()
+	defer outputFile.Close()
 
 	// Read files from folder to check how many are available
-	files, err := ioutil.ReadDir(input_folderpath)
+	files, err := ioutil.ReadDir(inputFolderpath)
 	if err != nil {
 		return err
 	}
 	lock := sync.Mutex{}
 
-	i_start := 1
-	i_end := len(files)
-	result_assemble := make(map[int][]byte, i_end)
+	iStart := 1
+	iEnd := len(files)
+	resultAssemble := make(map[int][]byte, iEnd)
 	sem := make(chan bool, c.NumThread+1)
 
 	// To reduce memory, we start a thread to stream memory to disk.
 	// It also needs to take a thread, so that the last files are written before the program exits.
-	go func(current_i, end_i int, progress *int, output_file *os.File, result_assemble map[int][]byte, lock *sync.Mutex, sem chan bool) {
+	go func(iStart, iEnd int, progress *int, outputFile *os.File, resultAssemble map[int][]byte, lock *sync.Mutex, sem chan bool) {
 		sem <- true
 
-		stream_to_file(i_start, i_end, progress, output_file, result_assemble, lock, sem)
-	}(i_start, i_end, progress, output_file, result_assemble, &lock, sem)
+		streamToFile(iStart, iEnd, progress, outputFile, resultAssemble, lock, sem)
+	}(iStart, iEnd, progress, outputFile, resultAssemble, &lock, sem)
 
 	// Iterating over files
-	for i := i_start; i <= i_end; i++ {
+	for i := iStart; i <= iEnd; i++ {
 		sem <- true
 
 		// Reading and decoding in threads
-		go func(i int, result_assemble map[int][]byte, lock *sync.Mutex) {
+		go func(i int, resultAssemble map[int][]byte, lock *sync.Mutex) {
 			filename := c.FilePrefix + lpad(strconv.Itoa(i), "0", c.FilenamePadLength) + ".png"
-			f, err := os.Open(input_folderpath + string(os.PathSeparator) + filename)
+			f, err := os.Open(inputFolderpath + string(os.PathSeparator) + filename)
 			if err != nil {
 				panic(err)
 			}
@@ -168,9 +168,9 @@ func (c *converter) Decode(input_folderpath, output_filepath string, progress *i
 				panic(err)
 			}
 
-			decodeResult, err := decoder.Decode(bmp, decode_hints)
+			decodeResult, err := decoder.Decode(bmp, decodeHints)
 			if err != nil {
-				decodeResult, err = decoder.Decode(bmp, decode_hints2)
+				decodeResult, err = decoder.Decode(bmp, decodeHints2)
 
 				if err != nil {
 					panic(err)
@@ -178,11 +178,11 @@ func (c *converter) Decode(input_folderpath, output_filepath string, progress *i
 			}
 
 			lock.Lock()
-			result_assemble[i] = []byte(decodeResult.GetText())
+			resultAssemble[i] = []byte(decodeResult.GetText())
 			lock.Unlock()
 
 			<-sem
-		}(i, result_assemble, &lock)
+		}(i, resultAssemble, &lock)
 	}
 
 	// Wait for all threads
@@ -193,28 +193,28 @@ func (c *converter) Decode(input_folderpath, output_filepath string, progress *i
 	return nil
 }
 
-// stream_to_file writes content continuously from memory to disk to free memory
+// streamToFile writes content continuously from memory to disk to free memory
 // while maintaining the order when writing to file
-func stream_to_file(current_i, end_i int, progress *int, output_file *os.File, result_assemble map[int][]byte, lock *sync.Mutex, sem chan bool) {
-	for i := current_i; i <= end_i; i++ {
-		if chunk, ok := result_assemble[i]; ok {
-			tempProgress := int((current_i * 100) / end_i)
+func streamToFile(currentI, endI int, progress *int, outputFile *os.File, resultAssemble map[int][]byte, lock *sync.Mutex, sem chan bool) {
+	for i := currentI; i <= endI; i++ {
+		if chunk, ok := resultAssemble[i]; ok {
+			tempProgress := int((currentI * 100) / endI)
 			if tempProgress > *progress {
 				*progress = tempProgress
 			}
 
 			// Write to disk
-			output_file.Write(chunk)
+			outputFile.Write(chunk)
 
 			// Free memory
 			lock.Lock()
-			result_assemble[i] = nil
-			delete(result_assemble, i)
+			resultAssemble[i] = nil
+			delete(resultAssemble, i)
 			lock.Unlock()
 		} else {
 			// Not ready yet, sleep & restart
 			time.Sleep(2 * time.Second)
-			stream_to_file(i, end_i, progress, output_file, result_assemble, lock, sem)
+			streamToFile(i, endI, progress, outputFile, resultAssemble, lock, sem)
 			return
 		}
 	}
